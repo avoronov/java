13906c01ea3b1a75ad4f487ce37281e6f64ebef2
Alexander V <alexander-voronov@ya.ru>
2020-09-08 18:17:09 +0300

    first commit

---
+++ isStringValid/Main.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Set;
+
+
+public class Main {
+
+    private static boolean isValid(String str) {
+        Map<Character, Integer> charFrequency = new HashMap<Character, Integer>();
+        for (char ch : str.toCharArray()) {
+            if (charFrequency.containsKey(ch))
+                charFrequency.put(ch, charFrequency.get(ch) + 1);
+            else
+                charFrequency.put(ch, 1);
+        }
+        
+        Set<Integer> uniqueFreqs = new HashSet<Integer>(charFrequency.values());
+        
+        return uniqueFreqs.size() == 1;
+    }
+
+	public static void main(String[] args) {
+	    System.out.println("Enter string:");
+        Scanner input = new  Scanner(System.in);
+        while (input.hasNextLine()) {
+            String str = input.next();
+            if (Objects.equals("exit", str)) {
+                System.out.println("Bye!");
+                break;
+            }
+            if (isValid(str)) {
+                System.out.println("String is valid");
+            }
+            else {
+                System.out.println("String is not valid");
+            }
+        }	
+        input.close();	
+	}
+}
+
